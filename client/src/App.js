89496b3a4edbfc4c1b759ebc68081632412473b1
Binod Chaudhary <bunnycdy@gmail.com>
2022-05-04 15:36:07 +0545

    dispatch

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
 
 import digitallife from './images/digitallife.png';
 import Form from './components/Form/Form';
 import Posts from './components/Posts/Posts';
 import useStyles from './styles';
+import { getPosts } from './actions/posts';
 
 const App = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch]);
+    
     return (
         <Container maxWidth="lg">
             <AppBar position='static' color='inherit' className={classes.appBar}>
