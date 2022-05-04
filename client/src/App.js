15e017b110144dc1bf7504e43ec4710d9920e87d
Binod Chaudhary <bunnycdy@gmail.com>
2022-05-04 12:08:22 +0545

    Post, Posts and Form components created and imported

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+import digitallife from './images/digitallife.png';
+import Form from './components/Form/Form';
+import Posts from './components/Posts/Posts';
+
 const App = () => {
     return (
         <Container maxWidth="lg">
             <AppBar position='static' color='inherit'>
                 <Typography variant='h2' align='center'>Degitallife</Typography>
-                <img src='{digitallife}' alt='Ditallife' height={60}></img>
+                <img src={digitallife} alt="Digitallife" />
             </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
         </Container>
     )
 }
