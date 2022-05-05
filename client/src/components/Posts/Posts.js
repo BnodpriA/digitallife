e3ea4925664781d0a6de2cc4b50bba599c8406cf
Binod Chaudhary <bunnycdy@gmail.com>
2022-05-05 16:28:01 +0545

    posts fetched

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -9,11 +9,15 @@ const Posts = () => {
   const posts = useSelector((state) => state.posts);
   console.log(posts);
   return (
-    <>    
-        <h1>POSTS</h1>
-        <Post />
-        <Post />
-    </>
+    !posts.length ? <CircularProgress /> : (
+      <Grid className={classes.mainContainer} container alignItems="stretch" spacing={3}>
+          {posts.map((post) => (
+              <Grid key={post._id} item xs={12} sm={6}>
+                  <Post post={post}/>
+              </Grid>
+          ))}
+      </Grid>
+    )
   )
 }
 
