63bdb0efdcf367f8dae5e8112da68015d9d59a8b
Binod Chaudhary <bunnycdy@gmail.com>
2022-05-05 13:58:16 +0545

    create post

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import Post from './Post/Post';
+import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 
+import Post from './Post/Post';
 import useStyles from './styles';
 const Posts = () => {
   const classes = useStyles();
--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://digitallife:jbMC1wg4dzVfPQyj@realmcluster.qlasc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+// const CONNECTION_URL = 'mongodb://localhost:27017/mydbname';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL)
