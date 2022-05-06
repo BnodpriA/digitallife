bc65e38b049b621afdb08b3ff5c35d03bf731f62
Binod Chaudhary <bunnycdy@gmail.com>
2022-05-06 17:32:26 +0545

    padding

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -39,12 +39,12 @@ const Post = ({ post, setCurrentId }) => {
         <CardActions className={classes.cardActions}>
             <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}>
                 <ThumbUpAltIcon fontSize="small" />
-                Like
+                &nbsp; Like &nbsp;
                 {post.likeCount}
             </Button>
             <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
                 <DeleteIcon fontSize="small" />
-                Delete
+                &nbsp; Delete
             </Button>
         </CardActions>
     </Card>
