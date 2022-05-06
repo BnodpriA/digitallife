426cf9288005fe1e3bec9f1071d618bee22e44d2
Binod Chaudhary <bunnycdy@gmail.com>
2022-05-06 14:44:20 +0545

    delete functionality

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -8,6 +8,6 @@ const router = express.Router();
 router.get('/', getPosts);
 router.post('/', createPost);
 router.patch('/:id', updatePost);
-router.delete('.:id', deletePost);
+router.delete('/:id', deletePost);
 
 export default router;
