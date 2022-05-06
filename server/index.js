fd41211220950529d7d79d4704685d01a704c498
Binod Chaudhary <bunnycdy@gmail.com>
2022-05-06 18:14:06 +0545

    working with .env

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 
 const app = express();
+dotenv.config();
 
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -14,8 +16,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://digitallife:jbMC1wg4dzVfPQyj@realmcluster.qlasc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-// const CONNECTION_URL = 'mongodb://localhost:27017/mydbname';
+const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL)
