077878d44ba53e3743b21837efe93844e6afc0ad
Binod Chaudhary <bunnycdy@gmail.com>
2022-05-09 13:17:19 +0545

    Home and Auth component added

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import { AppBar, Typography } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core';
 
 import useStyles from './styles';
 import digitallife from '../../images/digitallife.png';
 
 const Navbar = () => {
     const classes = useStyles();
+    const user = null;
 
     return (
-        <AppBar position='static' color='inherit' className={classes.appBar}>
-            <img className={classes.image} src={digitallife} alt="Digitallife" height={70}/>
-            <Typography className={classes.heading} variant='h2' align='center'>Digitallife</Typography>
+        <AppBar position='static' color='inherit' className={classes.appBar}>           
+            <div className={classes.brandContainer}>
+                <img className={classes.image} src={digitallife} alt="Digitallife" height={70}/>
+                <Typography component={Link} to="/" className={classes.heading} variant='h2' align='center'>Digitallife</Typography>
+            </div>
+            <Toolbar className={classes.toolbar}>
+                {user ? (
+                    <div className={classes.profile}>
+                        <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
+                        <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
+                        <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
+                    </div>
+                ) : (
+                    <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+                )}
+            </Toolbar>         
         </AppBar>
     );
 }
